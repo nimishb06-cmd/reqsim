e05e0a1bf6bc5426d4c0104412d6738909486bae
harshitachaudhary2809-alt <harshitachaudhary2809@gmail.com>
2026-09-16 12:22:07 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,91 +1,62 @@
 #include <stdio.h>
 
-void fcfs()
+void priority()
 {
     int n;
-    int i;
-    int current = 0;
+    int i, j;
+    int temp;
 
-    int arrival[20];
-    int burst[20];
-    int completion[20];
-    int turnaround[20];
-    int waiting[20];
+    int id[20];
+    int priority[20];
+    int service[20];
 
-    float total_waiting = 0;
+    printf("\n===== PRIORITY SCHEDULING =====\n");
 
-    printf("\n===== FCFS EMERGENCY QUEUE =====\n");
+    printf("Higher number = higher priority\n");
 
     printf("Enter number of requests: ");
     scanf("%d", &n);
 
     for (i = 0; i < n; i++)
     {
-        printf("\nRequest %d arrival time: ", i + 1);
-        scanf("%d", &arrival[i]);
+        id[i] = i + 1;
 
-        printf("Request %d burst time: ", i + 1);
-        scanf("%d", &burst[i]);
+        printf("\nRequest %d service time: ", i + 1);
+        scanf("%d", &service[i]);
+
+        printf("Request %d priority: ", i + 1);
+        scanf("%d", &priority[i]);
     }
+
+    /* Sort according to priority */
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (priority[j] > priority[i])
+            {
+                temp = priority[i];
+                priority[i] = priority[j];
+                priority[j] = temp;
+
+                temp = service[i];
+                service[i] = service[j];
+                service[j] = temp;
+
+                temp = id[i];
+                id[i] = id[j];
+                id[j] = temp;
+            }
+        }
+    }
+
+    printf("\nRequest\tPriority\tService Time\n");
 
     for (i = 0; i < n; i++)
     {
-        if (current < arrival[i])
-            current = arrival[i];
-
-        current = current + burst[i];
-
-        completion[i] = current;
-
-        turnaround[i] =
-            completion[i] - arrival[i];
-
-        waiting[i] =
-            turnaround[i] - burst[i];
-
-        total_waiting += waiting[i];
+        printf("R%d\t%d\t\t%d\n",
+               id[i],
+               priority[i],
+               service[i]);
     }
-    printf("\nGANTT CHART\n\n");
-
-    printf(" ");
-    for(i = 0; i < n; i++)
-        printf("-------");
-    printf("-\n");
-
-    printf("|");
-    for(i = 0; i < n; i++)
-        printf("  R%d  |", i + 1);
-    printf("\n ");
-
-    for(i = 0; i < n; i++)
-        printf("-------");
-    printf("-\n");
-
-    current = 0;
-    printf("%d", current);
-
-    for(i = 0; i < n; i++)
-    {
-        if(current < arrival[i])
-            current = arrival[i];
-
-        current += burst[i];
-        printf("      %d", current);
-    }
-
-    printf("\nREQ\tAT\tBT\tCT\tTAT\tWT\n");
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
-               i + 1,
-               arrival[i],
-               burst[i],
-               completion[i],
-               turnaround[i],
-               waiting[i]);
-    }
-
-    printf("\nAverage Waiting Time = %.2f\n",
-           total_waiting / n);
 }
